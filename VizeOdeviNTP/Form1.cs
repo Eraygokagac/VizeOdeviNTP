ab01e41675d81987176cc51a1204a74a9dafc77d
Eraygokagac <65739821+Eraygokagac@users.noreply.github.com>
2021-04-24 19:39:42 +0300

    Class olusturuldu, internetten veriyi aldık.

--- VizeOdeviNTP/Form1.cs
+++ VizeOdeviNTP/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace VizeOdeviNTP
 {
@@ -17,8 +19,12 @@ namespace VizeOdeviNTP
             InitializeComponent();
         }
 
+        XElement root;
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            XDocument doc = XDocument.Load("https://data.ibb.gov.tr/en/dataset/0efb7d74-fffe-4cb5-b675-75086f3d7e29/resource/ae6be6d3-ef7b-4eca-a356-9f90c745a8e8/download/tedaviedilenhayvanlar.xml");
+            root = doc.Root;
 
         }
     }
